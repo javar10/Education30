d7590575e19b94e38d2baa60263166371899f9ae
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-02-25 14:49:50 -0500

    Update script.js
    Made edits after code review and testing.

--- 12 - Guess My Number/script.js
+++ 12 - Guess My Number/script.js
@@ -1,37 +1,42 @@
-const pressed = [];
+const pressedKeysArray = [];
 const secretCode = Math.floor(Math.random() * 100);
 const alertMessage = 'Enter a number between 0 and 99';
 let numGuesses = 0;
 
-
-// function to check the number
+// Get the guess as a number.
 function getGuess() {
-    let guess;
-    if (pressed[0] === 'Enter') {
-        alert(alertMessage);
-    } else if (pressed[2] === 'Enter') {
-        guess = pressed.splice(0, 2).join('');
-        moveBar(guess);
-    } else if (pressed[1] === 'Enter') {
-        guess = pressed.splice(0, 1).join('');
-        moveBar(guess);
-    }
-    highOrLow(guess);
+    const guess = pressedKeysArray.splice(0, pressedKeysArray.length - 1).join('');
+    return Number(guess);
 }
 
-// function to say higher or lower
-function highOrLow(guess) {
+// Move the estimation bar.
+const estimationBar = document.querySelector('.estimation');
+let low = 0;
+let high = 100;
+function moveBar(guess) {
+    if (guess < secretCode && guess > low) {
+        low = guess;
+    } else if (guess > secretCode && guess < high) {
+        high = guess;
+    }
+    estimationBar.style.left = `${low / 100 * 90 + 5}vw`;
+    estimationBar.style.width = `${(high - low) / 100 * 90}vw`;
+    estimationBar.style.display = 'block';
+}
+
+// Compare guess to secret code.
+function calcHighOrLow(guess) {
     const h3 = document.querySelector('h3');
-    if (Number(guess) > Number(secretCode)) {
+    if (guess > secretCode) {
         h3.textContent = `${guess} is too high!`;
         newTick(guess);
-        numGuesses ++;
-    } else if (Number(guess) < Number(secretCode)) {
+        numGuesses++;
+    } else if (guess < secretCode) {
         h3.textContent = `${guess} is too low!`;
         newTick(guess);
-        numGuesses ++;
-    } else if (Number(guess) == Number(secretCode)) {
-        numGuesses ++;
+        numGuesses++;
+    } else if (guess == secretCode) {
+        numGuesses++;
         document.querySelector('body').innerHTML = `
         <div class="correct">
         <h1>You guessed the secret number
@@ -45,60 +50,45 @@ function highOrLow(guess) {
         restart.addEventListener('click', () => location.reload());
     } else {
         alert(alertMessage);
+        console.log('high low else code ran');
     }
 }
 
-// function to move the estimation bar
-const estimationBar = document.querySelector('.estimation');
-let low = 0;
-let high = 100;
-function moveBar(guess) {
-    if (Number(guess) < Number(secretCode) && Number(guess) > low) {
-        low = Number(guess);
-    } else if (Number(guess) > Number(secretCode) && Number(guess) < high) {
-        high = Number(guess);
-    }
-    estimationBar.style.left = `${low/100*90+5}vw`;
-    estimationBar.style.width = `${(high-low)/100*90}vw`;
-    estimationBar.style.display = 'block';
-}
-
-// display ticks for each guess
+// Display a tick for each guess.
 function newTick(guess) {
     const newTick = document.createElement('div');
     const timeline = document.querySelector('.timeline');
     newTick.innerHTML = `<p>${guess}</p>`;
     newTick.classList.add('tick');
-    newTick.style.left = `${guess/100*90+5}vw`;
+    newTick.style.left = `${guess / 100 * 90 + 5}vw`;
     timeline.appendChild(newTick);
 }
 
-// storing pressed keys in an array
+// Reset array.
+function resetGuess() {
+    pressedKeysArray.splice(0, 3);
+    keyCount = 0;
+}
+
+// Listen for keypress event and store keys in an array.
 let keyCount = 0;
 window.addEventListener("keyup", (e) => {
-    if (!isNaN(e.key)) {
-        pressed.push(e.key);
+    if (!isNaN(parseInt(e.key))) {
+        pressedKeysArray.push(e.key);
         keyCount += 1;
-    } else if (e.key === 'Enter') {
-        if (pressed.length != 0) {
-            pressed.push(e.key);
-        }
-        getGuess();
-        pressed.splice(0,3);
-        keyCount = 0;
+    } else if (keyCount != 0 && e.key === 'Enter') {
+        pressedKeysArray.push(e.key);
+        const guess = getGuess();
+        moveBar(guess);
+        calcHighOrLow(guess);
+        resetGuess();
     } else {
+        resetGuess();
         alert(alertMessage);
-        keyCount = 0;
-    }
-    if (keyCount > 2 && e.key != 'Enter') {
-        pressed.splice(0,3);
-        alert(alertMessage);
-        keyCount = 0;
     }
 
-    if (pressed.length > 3) {
-        pressed.splice(0, 1);
+    if (keyCount > 2 && e.key != 'Enter') {
+        resetGuess();
+        alert(alertMessage);
     }
 });
-
-
